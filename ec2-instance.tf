d128306edf709527685539e8b64fc3fe201ad291
Kaano1 <ahmetyumusakdiken13@gmail.com>
2024-11-21 10:47:24 +0300

    Develope (in ec2-instance.tf): added worker_count for worker nodes count. we need to dynamic instance. right now we can set this attribute and we can set up how many we want node.

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,3 +1,9 @@
+variable "worker_count" {
+  description = "The number of Worker Node to create"
+  type        = number
+  default     = 2
+}
+
 resource "aws_instance" "master_node" {
 	ami = "ami-08eb150f611ca277f"
 	vpc_security_group_ids = [aws_security_group.control_plane.id]
@@ -10,17 +16,16 @@ resource "aws_instance" "master_node" {
 	}
 }
 
-/*
-resource "aws_instance" "worker-node" {
+
+resource "aws_instance" "worker_node" {
 	ami = "ami-08eb150f611ca277f"
 	vpc_security_group_ids = [aws_security_group.worker_node.id]
 	subnet_id = aws_subnet.subnet_public_ip.id
 	instance_type = "t3.medium"
-	count = 2
+	count = var.worker_count
 	key_name = "myComputer"
 
 	tags = {
-		Name = "worker-node"
+		Name = "worker_node"
 	}
 }
-*/
